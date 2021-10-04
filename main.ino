2c0416c891929faee6b3ebfd0e015960b2fbcd1a
Jonny <faane784@gmail.com>
2021-10-03 22:30:05 -0700

    Initial commit

---
+++ main.ino
@@ -0,0 +1,19 @@
+#include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
+// #include <avr/power.h>
+
+Adafruit_NeoPixel pixels(16, 6, NEO_GRB + NEO_KHZ800);
+
+/**
+ * Runs once to setup any variables and classes.
+ */
+void setup()
+{
+}
+
+/**
+ * Runs every cycle.
+ */
+void loop()
+{
+}
